f5cd798c80d3d3226d945e7278831b939b097167
Amaljith <amaljith6430@gmail.com>
2025-01-16 00:59:15 +0530

    finally workings112039

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,26 @@
-
-export default {
-  'users-permissions': {
-    config: {
-      jwtSecret: '+tzGL61SzkKOcHT1QO3Uew==',  // Set your JWT secret here
+export default ({ env }) => ({
+    email: {
+      config: {
+        provider: 'gmail',  // Use 'smtp' if using a custom SMTP provider
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: env('EMAIL_USERNAME', 'luminarylines@gmail.com'),  // Your Gmail email
+            pass: env('EMAIL_PASSWORD', '3LzwNMHxnGBff5L'),  // Use app-specific password for Gmail
+          },
+        },
+        settings: {
+          defaultFrom: 'luminarylines@gmail.com',  // Your email
+          defaultReplyTo: 'luminarylines@gmail.com',  // Your email
+        },
+      },
     },
-  },
-};
+  
+    // Users Permissions Plugin Configuration
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', '+tzGL61SzkKOcHT1QO3Uew=='),  // JWT Secret for authentication
+      },
+    },
+  });
