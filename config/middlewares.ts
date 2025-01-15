286ce6b9c9ceda5ebc4df973fd422441cda8bca0
Amaljith <amaljith6430@gmail.com>
2025-01-16 02:14:32 +0530

    come

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,20 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000', // Local development (Next.js default)
+        'https://www.luminarylines.school', // Production frontend domain
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+      credentials: true, // If you're using cookies or credentials with requests
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
