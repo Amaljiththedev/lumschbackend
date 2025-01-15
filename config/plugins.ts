fc58dc967c0aa7d36bc9e177b709b3fe39744d06
Amaljith <amaljith6430@gmail.com>
2025-01-16 01:27:36 +0530

    come

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,26 +1,9 @@
 export default ({ env }) => ({
-    email: {
-      config: {
-        provider: 'gmail',  // Use 'smtp' if using a custom SMTP provider
-        providerOptions: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          auth: {
-            user: env('EMAIL_USERNAME', 'luminarylines@gmail.com'),  // Your Gmail email
-            pass: env('EMAIL_PASSWORD', '3LzwNMHxnGBff5L'),  // Use app-specific password for Gmail
-          },
-        },
-        settings: {
-          defaultFrom: 'luminarylines@gmail.com',  // Your email
-          defaultReplyTo: 'luminarylines@gmail.com',  // Your email
-        },
-      },
-    },
-  
+
     // Users Permissions Plugin Configuration
-    'users-permissions': {
+
       config: {
         jwtSecret: env('JWT_SECRET', '+tzGL61SzkKOcHT1QO3Uew=='),  // JWT Secret for authentication
-      },
+
     },
   });
