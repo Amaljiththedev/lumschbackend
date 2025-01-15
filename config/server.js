110518eaf0142f88d8d6e4e929f3a90f8eeb807c
Amaljith <amaljith6430@gmail.com>
2025-01-15 23:37:21 +0530

    finally working

--- config/server.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  keys: ['4c024ab47003387e60a0ab3bea8d3aaa648c217d397b1787c08a137bd9a738eeaacfe4fbd29970372adee85796afc7a15fcd951f1aa44c4e7d74d40feeca5eb9'],
-});
---
+++ config/server.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  host: '0.0.0.0',
+  port: 1337,
+  app: {
+    keys: [
+      'be16a8e2477ef5b631a6ca0ca2dd2d171a1b427d3f078cfd0cbcd5806d4a7d80',
+      '900215409256063cd90f357b2f59b0644d09a461ebb54df8b510164a651c8f8',
+    ],
+  },
+});
