5d2230a2fdf3821aaef6622c4f35bba9d67d0f3a
Amaljith <amaljith6430@gmail.com>
2025-01-15 22:37:35 +0530

    backend config changed

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+module.exports = {
+  host: process.env.HOST || '0.0.0.0',
+  port: process.env.PORT || 1337,
+  url: process.env.URL || 'http://localhost:1337',
+  admin: {
+    auth: {
+      secret: process.env.ADMIN_JWT_SECRET || 'your-admin-jwt-secret',
+    },
   },
-});
+  keys: process.env.APP_KEYS ? process.env.APP_KEYS.split(',') : ['yourRandomKey1', 'yourRandomKey2'], // Set app keys
+};
