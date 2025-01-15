2daaab0547b3cf116aed6fcca6952132b044e8c6
Amaljith <amaljith6430@gmail.com>
2025-01-15 22:41:18 +0530

    backend config app keys

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,7 @@
 module.exports = {
-  host: process.env.HOST || '0.0.0.0',
-  port: process.env.PORT || 1337,
-  url: process.env.URL || 'http://localhost:1337',
-  admin: {
-    auth: {
-      secret: process.env.ADMIN_JWT_SECRET || 'your-admin-jwt-secret',
-    },
-  },
-  keys: process.env.APP_KEYS ? process.env.APP_KEYS.split(',') : ['yourRandomKey1', 'yourRandomKey2'], // Set app keys
+  host: '0.0.0.0',
+  port: 1337,
+  keys: [
+    '717d344ed0be6e7cad297212b2bfd26e218a0a4e32d584927da7dc88ce7f6b73ffdd451b45b324d15d371795e7e68d3ae81da35de602def3f8851bc442c75ffc',
+  ],
 };
