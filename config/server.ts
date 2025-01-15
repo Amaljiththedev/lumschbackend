02ad403d26d77f3521825a64996a31552adc1868
Amaljith <amaljith6430@gmail.com>
2025-01-15 23:09:53 +0530

    backend config app keys

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  keys: ['b369e09c9d5c6272bea8cc79ebcb490945bcc1000eac4a7eed61b9a79ce87f56d13d72fc2415537e7fc211a565200bd49e9b53e1b7c7460395727b033a4455ac'],
+  host: '0.0.0.0',        // Static host, no need for env variables
+  port: 1337,             // Static port, no need for env variables
+  keys: ['b369e09c9d5c6272bea8cc79ebcb490945bcc1000eac4a7eed61b9a79ce87f56d13d72fc2415537e7fc211a565200bd49e9b53e1b7c7460395727b033a4455ac'], // Static app keys
 });
